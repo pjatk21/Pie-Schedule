72911662101b60852278aefc7934c924b7ec6c1f
Krystian Postek <krystian@postek.eu>
2022-02-21 18:34:45 +0100

    Initial Commit

---
+++ Pie Schedule/Pie_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pie_ScheduleApp.swift
+//  Pie Schedule
+//
+//  Created by Krystian Postek on 21/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pie_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
