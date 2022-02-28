5008d100823f9a2be633d64ca7e1474831501a6c
Krystian Postek <krystian@postek.eu>
2022-02-28 12:10:30 +0100

    Update welcome sheet

--- Pie Schedule/ContentView.swift
+++ Pie Schedule/ContentView.swift
@@ -69,7 +69,12 @@ struct ContentView: View {
     
     private let pages = [
         WelcomeSheetPage(title: "Pie Schedule", rows: [
-            WelcomeSheetPageRow(imageSystemName: "calendar", title: "Twoja nowa aplikacja do planu zajęć", content: "Już nigdy nie tykaj tej webowej apki z 2010")
+            WelcomeSheetPageRow(imageSystemName: "calendar", title: "Twoja nowa aplikacja do planu zajęć", content: "Już nigdy nie tykaj tej webowej apki z 2010"),
+            WelcomeSheetPageRow(imageSystemName: "hare", title: "Ekstermalnie szybkie API", content: "Czas odpowiedzi API z zajęciami jest nawet 90x krótszy niż czas odpowiedzi strony z planem zajęć. Nie bój się, że nie będziesz mógł/mógła sprawdzić, swojego planu zajęć.")
+        ]),
+        WelcomeSheetPage(title: "Pie Schedule", rows: [
+            WelcomeSheetPageRow(imageSystemName: "airplane.departure", title: "Zawsze na czas", content: "Aplikacja uwzględnia twoją aktualną strefę czasową, nie musisz już przeliczać kiedy rozpoczyna się wykład."),
+            WelcomeSheetPageRow(imageSystemName: "character.bubble", title: "Poliglota", content: "Aplikacja dzięki wsparciu społeczności została przetłumacznona na takie jęzki jak ukraiński/białoruski")
         ])
     ]
 }
