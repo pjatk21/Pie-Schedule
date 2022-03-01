05db41e3fbced30932341a5f721fce0c24346d9a
Krystian Postek <krystian@postek.eu>
2022-03-01 19:16:22 +0100

    Move realm into app group

--- Pie Schedule/RealmConfiguration.swift
+++ Pie Schedule/RealmConfiguration.swift
@@ -12,7 +12,8 @@ extension Realm.Configuration {
     static let previewConfig: Realm.Configuration = .init(inMemoryIdentifier: "preview")
     static let devConfig: Realm.Configuration = .init(deleteRealmIfMigrationNeeded: true)
     static let prodConfig: Realm.Configuration = {
-        let realmLocation = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.\(Bundle.main.bundleIdentifier!)")!
+        var realmLocation = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.\(Bundle.main.bundleIdentifier!)")!
+        realmLocation.appendPathComponent("pieschedule.realm")
         return Realm.Configuration(fileURL: realmLocation, deleteRealmIfMigrationNeeded: true)
     }()
 }
