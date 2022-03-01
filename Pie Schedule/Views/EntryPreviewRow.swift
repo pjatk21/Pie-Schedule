dda53c0d2888e5f090b491bf576e3f61c6b0f6de
Krystian Postek <krystian@postek.eu>
2022-03-01 23:51:28 +0100

    Update preview row view
    Set multiline alignment, spacer with minWidth

--- Pie Schedule/Views/EntryPreviewRow.swift
+++ Pie Schedule/Views/EntryPreviewRow.swift
@@ -19,14 +19,16 @@ struct EntryPreviewRow: View {
                     .font(.system(size: 24, weight: .bold, design: .rounded))
                 Text(entry.begin.formatted(date: .omitted, time: .shortened) + " - " + entry.end.formatted(date: .omitted, time: .shortened))
             }
-            Spacer()
+            Spacer(minLength: 30)
             VStack(alignment: .trailing) {
                 Text((entry.name))
                     .font(.system(size: 12))
                     .italic()
+                    .multilineTextAlignment(.trailing)
                 Text((entry.type))
                     .font(.system(size: 12))
                     .italic()
+                    .multilineTextAlignment(.trailing)
                 if let tutor = entry.tutor {
                     Text(tutor)
                         .font(.system(size: 12))
@@ -37,11 +39,12 @@ struct EntryPreviewRow: View {
     }
 }
 
+
 struct EntryPreviewRow_Previews: PreviewProvider {
     static var previews: some View {
-        let p = try! JSONDecoder().decode(ScheduleEntry.self, from: "{\"begin\":\"2022-03-07T15:45:00.000Z\",\"end\":\"2022-03-07T17:15:00.000Z\",\"dateString\":\"2022-03-07\",\"type\":\"Ćwiczenia\",\"code\":\"PJC\",\"name\":\"Programowanie w językach C i C++\",\"room\":\"A/357\",\"tutor\":null,\"groups\":[{\"location\":\"W\",\"mainSubject\":\"I\",\"studyMode\":\"s\",\"level\":\"I\",\"semester\":2,\"itn\":false,\"groupNumber\":46,\"groupLetter\":\"c\",\"raw\":\"WIs I.2 - 46c\"}],\"building\":\"A2020\"}".data(using: .utf8)!)
-        return List {
-            EntryPreviewRow(entry: p)
+        List {
+            EntryPreviewRow(entry: .loremIpsum)
         }
     }
 }
+
